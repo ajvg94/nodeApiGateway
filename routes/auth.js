e0dd67ea7a4da573b62de42c01fee450433180ca
ajvg94 <atiliovg94@gmail.com>
2020-08-31 19:05:34 -0400

    updated dependencies, and security

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,7 @@
     const User = require('../model/User');
     const {validateUser}= require('../model/User');
     const bcrypt = require('bcrypt');
+    const jwt = require('jsonwebtoken');
 //#endregion
 //----------------------------------------------------------------------------------------------------------------------
 //#region routes 
@@ -23,21 +24,9 @@
         const salt = await bcrypt.genSalt(7);
         const hashPassword = await bcrypt.hash(req.body.secret, salt);
 
-        //create user
-        const user = new User({
-            username: req.body.username,
-            secret: hashPassword
-        });
-        try{
-            const savedUser = await user.save();
-            res.send(savedUser._id);
-        }catch (e){
-            res.status(400).send(e);
-        }
-
     });
 
-    router.post('/login', (req,res)=>{
+    router.post('/login', async (req,res) => {
         console.log("");
         console.log(req.body);
 
@@ -45,7 +34,20 @@
         const { error } = validateUser(req.body);
         if(error) return res.status(400).send(error.details[0].message);
 
-        
+        //username exists
+        const user = await User.findOne({ username: req.body.username });
+        if(!user) return res.status(400).send("Wrong username or secret");
+        //if(!user) return res.status(400).send("Wrong username");
+
+        const validPass = await bcrypt.compare(req.body.secret, user.secret);
+        if(!validPass) return res.status(400).send("Wrong username or secret");
+        //if(!validPass) return res.status(400).send("Wrong secret");
+
+        //Create token
+        const token = jwt.sign({_id: user._id});
+
+        res.send('Logged in!');
+
     });
 //#endregion
 //----------------------------------------------------------------------------------------------------------------------
--- server.js
+++ server.js
@@ -5,18 +5,57 @@
     const mongoose = require('mongoose');
     const bodyParser = require('body-parser');
     require ('dotenv').config();
+    const rateLimit = require("express-rate-limit");
+    const slowDown = require("express-slow-down");
+    const hpp = require('hpp');
+    const helmet = require("helmet");
+    var cors = require('cors') ;
 //#endregion
 //----------------------------------------------------------------------------------------------------------------------
+//#region APP-CONFIG 
 //Express 
     const app = express();
     app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({extended: true}));
-    app.set('trust proxy',1);
+    app.use(bodyParser.urlencoded());
+    app.enable("trust proxy");
 //DB
     mongoose.connect(process.env.MONGO_DB_URL,{ useUnifiedTopology: true, useNewUrlParser: true},
         () => console.log('connected'));
 
-//rutas
+//Rate-limit
+    const limiter = rateLimit({
+        windowMs: 15 * 60 * 1000, // 15 minutes
+        max: 100 // limit each IP to 100 requests per windowMs
+    });
+    app.use(limiter);
+
+//Slow-down
+    const speedLimiter = slowDown({
+        windowMs: 15 * 60 * 1000, // 15 minutes
+        delayAfter: 100, // allow 100 requests per 15 minutes
+        delayMs: 500 //increase 500ms each request
+    });
+    app.use(speedLimiter);
+
+//HPP
+    app.use(hpp());
+
+//Too Busy
+    app.use(function(req, res, next) {
+        if (toobusy()) {
+            res.send(503, "Server busy try again later.");
+        } else {
+            next();
+        }
+    });
+//Helmet
+    app.use(helmet());
+
+//Cors
+    app.use(cors());
+//#endregion
+//----------------------------------------------------------------------------------------------------------------------
+//RUTAS
     app.use('/api/user', authRoute);
 //----------------------------------------------------------------------------------------------------------------------
     const port = process.env.PORT;
